1349b6b8cbcb34084bca03986bb643fc63a3ab59
theJames <james.newton@cognizant.com>
2020-02-17 18:39:57 -0600

    added user login service

---
+++ AppApiUsers/src/main/java/com/tomaslerichedemo/AppApiUsers/AppApiUsersApplication.java
@@ -0,0 +1,13 @@
+package com.tomaslerichedemo.AppApiUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApiUsersApplication.class, args);
+	}
+
+}
---
+++ AppApiUsers/src/test/java/com/tomaslerichedemo/AppApiUsers/AppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomaslerichedemo.AppApiUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
